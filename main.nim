66480eae8c584181cd5dbdc0c9ee1a80dcfef708
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2016-12-22 17:09:14 +0100

    rm bin, helloworld

--- main.nim
+++ main.nim
@@ -24,6 +24,9 @@ proc spherical(pt: Point): Point =
   let r2 = pt.x*pt.x + pt.y*pt.y
   (x * sin(r2) - y*cos(r2) , x*cos(r2) + y*sin(r2) )
 
+proc symmetrical(pt: Point): Point =
+  (-pt.x, -pt.y)
+
 proc swirl(pt: Point): Point = 
   let (x,y) = pt
   let r2 = pt.x*pt.x + pt.y*pt.y
@@ -40,12 +43,55 @@ proc step(ifs: seq[IFS], p: Point): Point =
   let f = random(ifs)
   f.apply(p)
 
-proc chaos(ifs: seq[IFS], n: int, pts: seq[Point]): seq[Point] =
-  if (n == 0):
-    pts
+
+proc hslToRgb(h: float, s: float, l: float): (uint8, uint8, uint8) =
+  var r, g, b: float = 0.0
+
+  if(s == 0):
+    #achromatic
+    r = l
+    g = l
+    b = l
   else:
-    let pp = pts.map(proc(pt:Point):Point = step(ifs, pt))
-    (chaos(ifs, n-1, pp))
+    proc hue2rgb(p, q: float, tt: float): float =
+      var t = tt
+      if t < 0: t += 1
+      if t > 1: t -= 1
+      if t < 1/6: return p + (q - p) * 6 * t
+      if t < 1/2: return q
+      if t < 2/3: return p + (q - p) * (2/3 - t) * 6
+      return p
+
+    var q = if l < 0.5: 
+              l * (1 + s) 
+            else: 
+              l + s - l * s
+    var p = 2 * l - q
+    r = hue2rgb(p, q, h + 1/3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1/3)
+    
+
+
+  return ((r * 255).uint8, (g * 255).uint8, (b * 255).uint8) 
+
+
+
+
+# proc 
+#     rgb = hsl (fst thePalette + (hue'*(snd thePalette - (fst thePalette)))) (saturation params hue') (lightness params hue')
+#    		in makeColor (channelRed rgb) (channelGreen rgb) (channelBlue rgb) (alpha params hue')
+
+
+
+proc chaos(ifs: seq[IFS], n: int, pt: Point, color: float, acc: var seq[Point], colors: var seq[float]) =
+  if (n != 0):
+    #let pp = pts.map(proc(pt:Point):Point = step(ifs, pt))
+   let p = step(ifs, pt)
+   acc.add(p)
+   let c = 0.8*(color)
+   colors.add(c)
+   (chaos(ifs, n-1, p, c, acc, colors))
 
 #proc generate(ifss: seq[IFS], pt: Point)
 
@@ -73,13 +119,13 @@ proc sdlMain() =
           xc: ( s*cos(PI/4.0), -s*sin(PI/4.0), 0.0 ),
           yc: ( s*sin(PI/4.0),  s*cos(PI/4.0), 0.0 ),
           p: 0.5,
-          variation: proc(p: Point): Point = swirl(spherical(p))
+          variation: proc(p: Point): Point = spherical(swirl(p))
         ),
         IFS(
           xc: ( s*cos(3*PI/4.0), -s*sin(3*PI/4.0), 1.0),
           yc: ( s*sin(3*PI/4.0),  s*cos(3*PI/4.0), 0.0),
           p: 0.5,
-          variation: proc(p: Point): Point = swirl(spherical(p))
+          variation: symmetrical
         )
       ]
       # @[
@@ -88,7 +134,9 @@ proc sdlMain() =
       #    IFS(xc:(0.2,-0.26,0.0), yc:(0.23,0.22,1.6),  p: 0.07, variation: linear),
       #    IFS(xc:(-0.15,0.28,0.0),yc:(0.26,0.24,0.44),p: 0.07, variation: linear)
       # ]
-    pts = chaos(ifs, 40, newSeqWith(300000, (0.0,0.0)))
+  var pts: seq[Point] = @[]  
+  var colors: seq[float] = @[]
+  chaos(ifs, 300000, (0.0, 0.0), 0.0, pts, colors)
 
     
 
@@ -99,7 +147,16 @@ proc sdlMain() =
   #   let p = (random(2.0)-1.0,  random(2.0)-1.0)
   #   let pr = f.apply(p)
   
-  
+  render.setDrawColor 0,0,0,0xFF
+  render.clear
+  for pair in zip(pts,colors):
+    let (pr,c) = pair
+    let (r,g,b) = hslToRgb(c, 0.7, 0.5)
+    echo(c)
+    render.setDrawColor r,g,b,0xff
+    render.drawPoint((Width/2 + pr.x*Width/3).cint, Height-(Height/2 + pr.y*Height/3).cint)
+  render.present
+
   while runGame:
     while pollEvent(evt):
       if evt.kind == QuitEvent:
@@ -107,14 +164,8 @@ proc sdlMain() =
         break
 
     let dt = fpsman.getFramerate() / 1000
-    render.setDrawColor 0,0,0xFF,0xFF
-    render.clear
 
     
-    render.setDrawColor 0,0,0x00,0x00
-    for pr in pts:
-      render.drawPoint((Width/2 + pr.x*Width/3).cint, (Height/2 + pr.y*Height/3).cint)
-    render.present
     fpsman.delay
 
     # render.setDrawColor 0,0,0xFF,0xFF
